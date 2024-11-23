59f6c7142b21cd74ed8a236c4f79ab878cb1a2a3
Emanuel  Chirico <emanuelchirico03@gmail.com>
2024-11-23 11:28:37 +0100

    Aggiunta Classe2

---
+++ Progetto/src/main/java/it/unisa/diem/ingsoft/gruppo4/progetto/Classe2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.unisa.diem.ingsoft.gruppo4.progetto;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class Classe2 {
+    
+    private int variabile;
+     
+
+}
