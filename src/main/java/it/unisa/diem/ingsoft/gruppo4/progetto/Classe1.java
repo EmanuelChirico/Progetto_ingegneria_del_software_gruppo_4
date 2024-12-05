62fac97f3cb97447d723f22a107463108f0a141a
Emanuel  Chirico <emanuelchirico03@gmail.com>
2024-12-05 19:15:11 +0100

    commit src folder

---
+++ src/main/java/it/unisa/diem/ingsoft/gruppo4/progetto/Classe1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it.unisa.diem.ingsoft.gruppo4.progetto;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class Classe1 {
+    
+    static void main (String[]arg){
+    
+        System.out.println("ciao mondo");
+        System.out.println(" prova git ");
+    }
+
+}
