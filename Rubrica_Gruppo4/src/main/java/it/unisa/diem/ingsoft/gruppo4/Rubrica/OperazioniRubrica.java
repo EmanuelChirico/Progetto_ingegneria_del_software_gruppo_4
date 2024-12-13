711250209068c673bc84cb179f11269be09ad9c2
Luigii_11 <luigi11autorino@gmail.com>
2024-12-13 17:27:51 +0100

    Implementazione interfaccia OperazioniRubrica.java

--- Rubrica_Gruppo4/src/main/java/it/unisa/diem/ingsoft/gruppo4/Rubrica/OperazioniRubrica.java
+++ Rubrica_Gruppo4/src/main/java/it/unisa/diem/ingsoft/gruppo4/Rubrica/OperazioniRubrica.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.ingsoft.gruppo4.Rubrica;
 
+import java.util.List;
+import javafx.collections.ObservableList;
+
 /**
- *
- * @author 39329
+ * @file OperazioniRubrica.java
+ * @brief Interfaccia che definisce le operazioni principali per la gestione di una rubrica.
+ * @author Gruppo4
  */
 public interface OperazioniRubrica {
-    
+    boolean aggiungiContatto(Contatto c);
+    void rimuoviContatto(Contatto c);
+    boolean modificaContatto(Contatto contattoPrec, Contatto contattoMod);
+    List<Contatto> ricercaContatto(String str);
+    ObservableList<Contatto> getContatti();
+    void svuotaRubrica();
 }
+
